0789f3e222e3e7b6d301ad7d924148d65ded05e5
Strelnikov Ilia <ily_strelnikov@mail.ru>
2026-03-18 19:06:19 +0600

    HOMEWORK83 добавили функционал с паролем с использованием bcrypt

--- backend/src/models/User.ts
+++ backend/src/models/User.ts
@@ -1,5 +1,8 @@
 import mongoose from "mongoose";
 import {UserFields} from "../types";
+import bcrypt from "bcrypt";
+
+const SALT_WORK_FACTOR = 10;
 
 const UserSchema = new mongoose.Schema<UserFields>({
     username: {
@@ -16,6 +19,23 @@ const UserSchema = new mongoose.Schema<UserFields>({
         required: true,
     }
 });
+
+UserSchema.pre("save", async function(next) {
+    if (!this.isModified("password")) return;
+
+    const salt = await bcrypt.genSalt(SALT_WORK_FACTOR);
+    const hash = await bcrypt.hash(this.password, salt);
+
+    this.password = hash;
+});
+
+UserSchema.set('toJSON', {
+   transform: (_doc, ret, _options) => {
+       const {password, ...rest} = ret;
+       return rest;
+   }
+});
+
 const User = mongoose.model("User", UserSchema);
 
 export default User;
