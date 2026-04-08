cf4007c029f15dc132f091cda6058d83f2de78dd
Strelnikov Ilia <ily_strelnikov@mail.ru>
2026-04-08 20:27:34 +0600

    (HOMEWORK 89) добавлены юзеры и история в фиктсруы

--- backend/src/fixtures.ts
+++ backend/src/fixtures.ts
@@ -1,7 +1,10 @@
 import mongoose from "mongoose";
+import { randomUUID } from "node:crypto";
 import Artist from "./models/Artist";
 import Album from "./models/Album";
 import Track from "./models/Track";
+import User from "./models/User";
+import TrackHistory from "./models/TrackHistory";
 
 const run = async () => {
     await mongoose.connect("mongodb://127.0.0.1:27017/music-api");
@@ -12,6 +15,8 @@ const run = async () => {
         await db.dropCollection("artists");
         await db.dropCollection("albums");
         await db.dropCollection("tracks");
+        await db.dropCollection("users");
+        await db.dropCollection("trackhistories");
     } catch {}
 
     const [artist1, artist2] = await Artist.create(
@@ -46,7 +51,7 @@ const run = async () => {
         }
     );
 
-    await Track.create(
+    const [track1, track2, track3] = await Track.create(
         {
             title: "Track One",
             album: album1._id,
@@ -67,6 +72,50 @@ const run = async () => {
             number: 1,
             duration: "2:58",
             isPublished: false,
+        }
+    );
+
+    const [user1, user2, admin] = await User.create(
+        {
+            username: "user1",
+            password: "123",
+            token: randomUUID(),
+            role: "user",
+        },
+        {
+            username: "user2",
+            password: "123",
+            token: randomUUID(),
+            role: "user",
+        },
+        {
+            username: "admin",
+            password: "admin123",
+            token: randomUUID(),
+            role: "admin",
+        }
+    );
+
+    await TrackHistory.create(
+        {
+            user: user1._id,
+            track: track1._id,
+            datetime: new Date(),
+        },
+        {
+            user: user1._id,
+            track: track2._id,
+            datetime: new Date(),
+        },
+        {
+            user: user2._id,
+            track: track1._id,
+            datetime: new Date(),
+        },
+        {
+            user: user2._id,
+            track: track3._id,
+            datetime: new Date(),
         }
     );
 
